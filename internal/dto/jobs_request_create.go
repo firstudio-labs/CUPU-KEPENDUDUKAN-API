04979ac89a247377a2e73c730058be6689809e8d
koriebruh <koriebruh@gmail.com>
2025-03-10 20:28:13 +0700

    Update jobs_request_create.go

--- internal/dto/jobs_request_create.go
+++ internal/dto/jobs_request_create.go
@@ -1,6 +1,6 @@
 package dto
 
 type JobReqCreate struct {
-	Code string `validate:"required,min=5" json:"code"`
-	Name string `validate:"required,min=5" json:"name"`
+	Code string `validate:"required,min=1" json:"code"`
+	Name string `validate:"required,min=1 json:"name"`
 }
